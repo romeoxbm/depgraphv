d70c4a529e82d02bdedcbf8df6e0e4091813c05f
Francesco Guastella <guastella.francesco@gmail.com>
2013-11-24 18:26:13 +0100

    - Every function in main.cpp have been moved into depgraphV namespace;

--- src/main.cpp
+++ src/main.cpp
@@ -31,27 +31,34 @@
 #include <stdio.h>
 
 #ifdef WIN32
-void toggleConsole( bool enabled, bool pause );
+#	define WIN32_LEAN_AND_MEAN
+#	include <windows.h>
 #endif
 
-void printVersion()
+namespace depgraphV
 {
 #ifdef WIN32
-	toggleConsole( true, true );
+	void toggleConsole( bool enabled, bool pause );
 #endif
-	printf( "%s version %s\n", APP_NAME, APP_VER );
-}
 
-void printHelp()
-{
-	printVersion();
-	printf( "Usage\n\n" );
-	printf( "\t%s [options]\n\n", APP_NAME );
-	printf( "Options\n" );
-	printf( "\t-h (--help) \t\t= Print this help message and quit.\n" );
-	printf( "\t-v (--version) \t\t= Print %s version and quit.\n", APP_NAME );
-	printf( "\t-l (--with-log)\t\t= Enable log messages (disabled by default).\n" );
-}
+	void printVersion()
+	{
+#ifdef WIN32
+		toggleConsole( true, true );
+#endif
+		printf( "%s version %s\n", APP_NAME, APP_VER );
+	}
+
+	void printHelp()
+	{
+		printVersion();
+		printf( "Usage\n\n" );
+		printf( "\t%s [options]\n\n", APP_NAME );
+		printf( "Options\n" );
+		printf( "\t-h (--help) \t\t= Print this help message and quit.\n" );
+		printf( "\t-v (--version) \t\t= Print %s version and quit.\n", APP_NAME );
+		printf( "\t-l (--with-log)\t\t= Enable log messages (disabled by default).\n" );
+	}
 
 #ifdef QT_USE_QT5
 	void noMessageOutput( QtMsgType, const QMessageLogContext&, const QString& ) {}
@@ -60,50 +67,49 @@ void printHelp()
 #endif
 
 #ifdef WIN32
-#define WIN32_LEAN_AND_MEAN
-#include <windows.h>
-
-void toggleConsole( bool enabled, bool pause = false )
-{
-	bool initially = GetConsoleWindow() != NULL;
-	bool success = true;
-
-	if( enabled )
+	void toggleConsole( bool enabled, bool pause = false )
 	{
-		if( initially )
-			return;
+		bool initially = GetConsoleWindow() != NULL;
+		bool success = true;
 
-		if( AllocConsole() )
+		if( enabled )
 		{
-			SetConsoleTitle( APP_NAME );
-			freopen( "CONOUT$", "w", stdout );
-			freopen( "CONOUT$", "w", stderr );
+			if( initially )
+				return;
+
+			if( AllocConsole() )
+			{
+				SetConsoleTitle( APP_NAME );
+				freopen( "CONOUT$", "w", stdout );
+				freopen( "CONOUT$", "w", stderr );
+			}
+			else
+				success = false;
 		}
-		else
-			success = false;
+		else if( initially )
+		{
+			if( pause )
+				system( "pause" );
+
+			success = FreeConsole();
+		}
+
+		if( !success )
+			throw new std::exception();
 	}
-	else if( initially )
+
+	void _atExitFnc()
 	{
-		if( pause )
-			system( "pause" );
-
-		success = FreeConsole();
+		toggleConsole( false );
 	}
-
-	if( !success )
-		throw new std::exception();
-}
-
-void _atExitFnc()
-{
-	toggleConsole( false );
-}
+} //end of depgraphV namespace
 
 INT WINAPI WinMain( HINSTANCE, HINSTANCE, LPSTR, INT )
 {
-	atexit( _atExitFnc );
+	atexit( depgraphV::_atExitFnc );
 	QApplication app( __argc, __argv );
 #else
+} //end of depgraphV namespace
 
 int main( int argc, char* argv[] )
 {
@@ -122,34 +128,34 @@ int main( int argc, char* argv[] )
 			{
 				//Wrong option. Print help and quit
 				printf( "WRONG USAGE: Unknown option \"%s\"\n\n", app.arguments()[ i ].toStdString().c_str() );
-				printHelp();
+				depgraphV::printHelp();
 				return 0;
 			}
 		}
 
 		if( app.arguments().contains( "--version" ) || app.arguments().contains( "-v" ) )
 		{
-			printVersion();
+			depgraphV::printVersion();
 			return 0;
 		}
 
 		else if( app.arguments().contains( "--help" ) || app.arguments().contains( "-h" ) )
 		{
-			printHelp();
+			depgraphV::printHelp();
 			return 0;
 		}
 
 		else if( !app.arguments().contains( "--with-log" ) && !app.arguments().contains( "-l" ) )
 		{
 #ifdef QT_USE_QT5
-			qInstallMessageHandler( &noMessageOutput );
+			qInstallMessageHandler( &depgraphV::noMessageOutput );
 #else
-			qInstallMsgHandler( &noMessageOutput );
+			qInstallMsgHandler( &depgraphV::noMessageOutput );
 #endif
 		}
 #ifdef WIN32
 		else
-			toggleConsole( true );
+			depgraphV::toggleConsole( true );
 #endif
 	}
 
