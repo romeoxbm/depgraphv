faf2056208f32844067d43ec1b8050cf98178162
Francesco Guastella <guastella.francesco@gmail.com>
2013-11-29 12:21:47 +0100

    - Italian translation file updated; - README updated;

--- i18n/dep-graphV_it.ts
+++ i18n/dep-graphV_it.ts
@@ -2,6 +2,59 @@
 <!DOCTYPE TS>
 <TS version="2.0" language="it_IT">
 <context>
+    <name>depgraphV::AboutDialog</name>
+    <message>
+        <location filename="../ui/aboutdialog.ui" line="80"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;dep-graphV is an useful tool to analize header dependendencies via graphs.&lt;/p&gt;&lt;p&gt;Written in C++ and powered by Qt and GraphViz, it creates a graph of your codebase&lt;br/&gt;by parsing every &lt;span style=&quot; font-style:italic;&quot;&gt;#include&lt;/span&gt; statement found in the project&apos;s root folder.&lt;/p&gt;&lt;p&gt;It is helpful when you need to analize and visualize the dependecy tree,&lt;br/&gt;highlightining cycles and &amp;quot;useless paths&amp;quot;, and It means, in short, that you could drastically&lt;br/&gt;reduce build times, especially when working on large-scale projects.&lt;/p&gt;&lt;p&gt;Report bugs and download new versions at &lt;a href=&quot;http://sourceforge.net/projects/depgraph&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;http://sourceforge.net/projects/depgraph&lt;/span&gt;&lt;/a&gt;.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;dep-graphV è un utile strumento per analizzare le dipendenze degli header attraverso grafi.&lt;/p&gt;&lt;p&gt;Scritto in C++ e &amp;quot;powered by&amp;quot; Qt e GraphViz, crea un grafo di inclusione dei tuoi sorgenti&lt;br/&gt;facendo un parsing di ogni direttiva&lt;span style=&quot; font-style:italic;&quot;&gt;#include&lt;/span&gt; trovata nella cartella di root del progetto.&lt;/p&gt;&lt;p&gt;Risulta utile quando è necessario analizzare e visualizzare l&apos;albero delle dipendenze,&lt;br/&gt;evidenziando cicli e &amp;quot;percorsi inutili&amp;quot;, e ciò vuol dire, in breve, che è possibile ridurre drasticamente&lt;br/&gt;i tempi di build, specialmente lavorando su progetti molto grandi.&lt;/p&gt;&lt;p&gt;Segnala bug e scarica nuove versioni da &lt;a href=&quot;http://sourceforge.net/projects/depgraph&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;http://sourceforge.net/projects/depgraph&lt;/span&gt;&lt;/a&gt;.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../ui/aboutdialog.ui" line="94"/>
+        <source>Authors</source>
+        <translation>Autori</translation>
+    </message>
+    <message>
+        <location filename="../ui/aboutdialog.ui" line="111"/>
+        <source>Todo</source>
+        <translation>Da fare</translation>
+    </message>
+    <message>
+        <location filename="../ui/aboutdialog.ui" line="128"/>
+        <source>Changelog</source>
+        <translation>Lista dei cambiamenti</translation>
+    </message>
+    <message>
+        <location filename="../ui/aboutdialog.ui" line="145"/>
+        <source>License Agreement</source>
+        <translation>Accordo di licenza</translation>
+    </message>
+    <message>
+        <location filename="../ui/aboutdialog.ui" line="159"/>
+        <source>Versions</source>
+        <translation>Versioni</translation>
+    </message>
+    <message>
+        <location filename="../ui/aboutdialog.ui" line="172"/>
+        <source>GraphViz Version:</source>
+        <translation>Versione GraphViz:</translation>
+    </message>
+    <message>
+        <location filename="../ui/aboutdialog.ui" line="186"/>
+        <source>Qt Version:</source>
+        <translation>Versione Qt:</translation>
+    </message>
+    <message>
+        <location filename="../ui/aboutdialog.ui" line="223"/>
+        <source>Close</source>
+        <translation>Chiudi</translation>
+    </message>
+    <message>
+        <location filename="../src/aboutdialog.cpp" line="66"/>
+        <source>Cannot read </source>
+        <translation>Non riesco a leggere </translation>
+    </message>
+</context>
+<context>
     <name>depgraphV::AppConfig</name>
     <message>
         <location filename="../src/appconfig.cpp" line="66"/>
@@ -48,11 +101,16 @@
     </message>
     <message>
         <location filename="../src/graph.cpp" line="336"/>
-        <source>Looking for available graphviz plugins...</source>
-        <translation>Sto cercando i plugin di GraphViz disponibili...</translation>
+        <source>Looking for available GraphViz plugins...</source>
+        <translation>Sto cercando plugin di GraphViz disponibili...</translation>
     </message>
     <message>
-        <location filename="../src/graph.cpp" line="344"/>
+        <location filename="../src/graph.cpp" line="360"/>
+        <source>Plugins found:</source>
+        <translation>Plugin trovati:</translation>
+    </message>
+    <message>
+        <location filename="../src/graph.cpp" line="345"/>
         <source>Plugins for kind &quot;%1&quot;:</source>
         <translation>Plugin per tipo &quot;%1&quot;:</translation>
     </message>
@@ -60,264 +118,260 @@
 <context>
     <name>depgraphV::MainWindow</name>
     <message>
-        <location filename="../src/mainwindow.cpp" line="97"/>
+        <location filename="../src/mainwindow.cpp" line="99"/>
         <source>ready</source>
         <translation>pronto</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="165"/>
+        <location filename="../src/mainwindow.cpp" line="167"/>
         <source>Select root folder</source>
         <translation>Seleziona la cartella radice</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="185"/>
+        <location filename="../src/mainwindow.cpp" line="189"/>
+        <source>Unable to render; Plugin not found.</source>
+        <translation>Impossibile fare il rendering; Plugin non trovato.</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="192"/>
         <source>No file has been found in selected folder; nothing to draw.</source>
         <translation>Nessun file è stato trovato nella cartella selezionata; niente da disegnare.</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="194"/>
+        <location filename="../src/mainwindow.cpp" line="201"/>
         <source>Clear graph</source>
         <translation>Pulisci grafo</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="195"/>
-        <location filename="../src/mainwindow.cpp" line="236"/>
+        <location filename="../src/mainwindow.cpp" line="202"/>
+        <location filename="../src/mainwindow.cpp" line="232"/>
         <source>Are you sure?</source>
         <translation>Sei sicuro?</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="218"/>
-        <source>Cannot read about informations.</source>
-        <translation>Non riesco a leggere le informazioni di about.</translation>
-    </message>
-    <message>
-        <location filename="../src/mainwindow.cpp" line="228"/>
-        <source>About...</source>
-        <translation></translation>
-    </message>
-    <message>
-        <location filename="../src/mainwindow.cpp" line="251"/>
+        <location filename="../src/mainwindow.cpp" line="247"/>
         <source>Renderer method changed</source>
         <translation>Modalità di render modificata</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="288"/>
+        <location filename="../src/mainwindow.cpp" line="284"/>
         <source>Cannot save image</source>
         <translation>Impossibile salvare l&apos;immagine</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="288"/>
-        <source>Unable to save graph as image. (Plugin issue)</source>
-        <translation>Non posso salvare il grafo come immagine. (Si è verificato un problema di plugin)</translation>
+        <location filename="../src/mainwindow.cpp" line="284"/>
+        <source>Unable to save graph as image; Plugin not found.</source>
+        <translation>Impossibile salvare il grafo come immagine; Plugin non trovato.</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="303"/>
+        <location filename="../src/mainwindow.cpp" line="299"/>
         <source>Select path and name of the image file</source>
         <translation>Seleziona percorso e nome del file immagine</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="312"/>
-        <location filename="../src/mainwindow.cpp" line="328"/>
+        <location filename="../src/mainwindow.cpp" line="308"/>
+        <location filename="../src/mainwindow.cpp" line="324"/>
         <source>File successfully saved.</source>
         <translation>File salvato con successo.</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="314"/>
+        <location filename="../src/mainwindow.cpp" line="310"/>
         <source>Save as image</source>
         <translation>Salva come immagine</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="314"/>
-        <location filename="../src/mainwindow.cpp" line="330"/>
+        <location filename="../src/mainwindow.cpp" line="310"/>
+        <location filename="../src/mainwindow.cpp" line="326"/>
         <source>Unable to save file</source>
         <translation>Impossibile salvare il file</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="321"/>
+        <location filename="../src/mainwindow.cpp" line="317"/>
         <source>Select path and name of the dot file</source>
         <translation>Seleziona percorso e nome del file dot</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="330"/>
+        <location filename="../src/mainwindow.cpp" line="326"/>
         <source>Save as dot</source>
         <translation>Salva come dot</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="435"/>
+        <location filename="../src/mainwindow.cpp" line="431"/>
         <source>Found locale %1 in %2</source>
         <translation>Trovata localizzazione %1 in %2</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="470"/>
+        <location filename="../src/mainwindow.cpp" line="473"/>
         <source>Translation file &quot;%1&quot; does not exists</source>
         <translation>File di traduzione &quot;%1&quot; non esistente</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="478"/>
+        <location filename="../src/mainwindow.cpp" line="481"/>
         <source>Switched to translation file %1 in %2</source>
         <translation>Selezionato file di tradizione %1 in %2</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="37"/>
+        <location filename="../ui/mainwindow.ui" line="37"/>
         <source>Folder</source>
         <translation>Cartella</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="49"/>
+        <location filename="../ui/mainwindow.ui" line="49"/>
         <source>Root folder</source>
         <translation>Cartella radice</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="76"/>
+        <location filename="../ui/mainwindow.ui" line="76"/>
         <source>...</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="85"/>
+        <location filename="../ui/mainwindow.ui" line="85"/>
         <source>Scan recursively</source>
         <translation>Scansiona ricorsivamente</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="101"/>
+        <location filename="../ui/mainwindow.ui" line="101"/>
         <source>Parse options</source>
         <translation>Opzioni di analisi</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="107"/>
+        <location filename="../ui/mainwindow.ui" line="107"/>
         <source>Parse headers</source>
         <translation>Analizza header</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="117"/>
+        <location filename="../ui/mainwindow.ui" line="117"/>
         <source>Parse sources</source>
         <translation>Analizza sorgenti</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="133"/>
+        <location filename="../ui/mainwindow.ui" line="133"/>
         <source>Headers filter</source>
         <translation>Filtri sugli header</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="139"/>
-        <location filename="../src/mainwindow.ui" line="225"/>
+        <location filename="../ui/mainwindow.ui" line="139"/>
+        <location filename="../ui/mainwindow.ui" line="225"/>
         <source>Standard filters</source>
         <translation>Filtri standard</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="184"/>
-        <location filename="../src/mainwindow.ui" line="275"/>
+        <location filename="../ui/mainwindow.ui" line="184"/>
+        <location filename="../ui/mainwindow.ui" line="275"/>
         <source>Custom (semicolon-separated)</source>
         <translation>Personalizzato (separati da ; )</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="219"/>
+        <location filename="../ui/mainwindow.ui" line="219"/>
         <source>Sources filter</source>
         <translation>Filtri sui sorgenti</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="332"/>
+        <location filename="../ui/mainwindow.ui" line="332"/>
         <source>Clear</source>
         <translation>Pulisci</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="342"/>
-        <location filename="../src/mainwindow.cpp" line="185"/>
+        <location filename="../ui/mainwindow.ui" line="342"/>
+        <location filename="../src/mainwindow.cpp" line="189"/>
+        <location filename="../src/mainwindow.cpp" line="192"/>
         <source>Draw</source>
         <translation>Disegna</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="393"/>
+        <location filename="../ui/mainwindow.ui" line="393"/>
         <source>&amp;File</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="402"/>
+        <location filename="../ui/mainwindow.ui" line="402"/>
         <source>&amp;Help</source>
         <translation>&amp;Aiuto</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="409"/>
+        <location filename="../ui/mainwindow.ui" line="409"/>
         <source>&amp;Options</source>
         <translation>&amp;Opzioni</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="413"/>
+        <location filename="../ui/mainwindow.ui" line="413"/>
         <source>&amp;Renderer</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="420"/>
+        <location filename="../ui/mainwindow.ui" line="420"/>
         <source>&amp;Language</source>
         <translation>&amp;Lingua</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="439"/>
+        <location filename="../ui/mainwindow.ui" line="439"/>
         <source>About &amp;Qt</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="450"/>
+        <location filename="../ui/mainwindow.ui" line="450"/>
         <source>&amp;Native</source>
         <translation>&amp;Nativo</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="453"/>
+        <location filename="../ui/mainwindow.ui" line="453"/>
         <source>Ctrl+R, Ctrl+N</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="461"/>
+        <location filename="../ui/mainwindow.ui" line="461"/>
         <source>&amp;OpenGL</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="464"/>
+        <location filename="../ui/mainwindow.ui" line="464"/>
         <source>Ctrl+R, Ctrl+O</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="472"/>
+        <location filename="../ui/mainwindow.ui" line="472"/>
         <source>High Quality &amp;Antialiasing</source>
         <translation>&amp;Antialiasing ad alta qualità</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="480"/>
+        <location filename="../ui/mainwindow.ui" line="480"/>
         <source>Save as &amp;image...</source>
         <translation>Salva come &amp;immagine...</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="483"/>
+        <location filename="../ui/mainwindow.ui" line="483"/>
         <source>Ctrl+I</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="491"/>
+        <location filename="../ui/mainwindow.ui" line="491"/>
         <source>&amp;Save as dot...</source>
         <translation>&amp;Salva come dot...</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="494"/>
+        <location filename="../ui/mainwindow.ui" line="494"/>
         <source>Ctrl+S</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="499"/>
+        <location filename="../ui/mainwindow.ui" line="499"/>
         <source>&amp;Exit</source>
         <translation>&amp;Esci</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="504"/>
+        <location filename="../ui/mainwindow.ui" line="504"/>
         <source>&amp;About</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="518"/>
+        <location filename="../ui/mainwindow.ui" line="518"/>
         <source>System language</source>
         <translation>Lingua di sistema</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="523"/>
-        <location filename="../src/mainwindow.cpp" line="235"/>
+        <location filename="../ui/mainwindow.ui" line="523"/>
+        <location filename="../src/mainwindow.cpp" line="231"/>
         <source>Restore default settings</source>
         <translation>Ripristina impostazioni di default</translation>
     </message>
