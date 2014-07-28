0e9e654e43737fbc436fee5161729cf17f1a0715
Francesco Guastella <guastella.francesco@gmail.com>
2014-07-28 13:20:28 +0200

    - Better handling of command line arguments in main.cpp; - Version command line arg now use a capital V;

--- src/main.cpp
+++ src/main.cpp
@@ -39,15 +39,15 @@ namespace depgraphV
 {
 #ifdef WIN32
 	/**
-	* @brief toggleConsole function allocate/free a log console. Only available on Windows.
-	* @param enabled True when a console allocation is required, false when the already allocated console should be freed.
-	*/
+	 * @brief toggleConsole function allocate/free a log console. Only available on Windows.
+	 * @param enabled True when a console allocation is required, false when the already allocated console should be freed.
+	 */
 	void toggleConsole( bool enabled );
 #endif
 
 	/**
-	* @brief Print the application version and quit.
-	*/
+	 * @brief Print the application version and quit.
+	 */
 	void printVersion()
 	{
 #ifdef WIN32
@@ -57,8 +57,8 @@ namespace depgraphV
 	}
 
 	/**
-	* @brief This function prints out an help message showing all the command line arguments available and their usage.
-	*/
+	 * @brief This function prints out an help message showing all the command line arguments available and their usage.
+	 */
 	void printHelp()
 	{
 		printVersion();
@@ -66,14 +66,14 @@ namespace depgraphV
 		printf( "\t%s [options]\n\n", APP_NAME );
 		printf( "Options\n" );
 		printf( "\t-h (--help) \t\t= Print this help message and quit.\n" );
-		printf( "\t-v (--version) \t\t= Print %s version and quit.\n", APP_NAME );
+		printf( "\t-V (--version) \t\t= Print %s version and quit.\n", APP_NAME );
 		printf( "\t-l (--with-log)\t\t= Enable log messages (disabled by default).\n" );
 	}
 
 	/**
-	* @brief This function is being used when it is necessary to disable log messages sent by this application
-	*		  via qDebug(), qWarning(), etc.
-	*/
+	 * @brief This function is being used when it is necessary to disable log messages sent by this application
+	 *		  via qDebug(), qWarning(), etc.
+	 */
 #ifdef QT_USE_QT5
 	void noMessageOutput( QtMsgType, const QMessageLogContext&, const QString& ) {}
 #else
@@ -94,6 +94,7 @@ namespace depgraphV
 			if( AllocConsole() )
 			{
 				SetConsoleTitle( APP_NAME );
+				//TODO WARNING C4996 here (freopen may be unsafe)
 				freopen( "CONOUT$", "w", stdout );
 				freopen( "CONOUT$", "w", stderr );
 			}
@@ -111,9 +112,9 @@ namespace depgraphV
 	}
 
 	/**
-	* @brief Function called when quitting this application.
-	*/
-	void _atExitFnc()
+	 * @brief Function called when quitting this application.
+	 */
+	void _atExitFunc()
 	{
 		toggleConsole( false );
 	}
@@ -121,7 +122,7 @@ namespace depgraphV
 
 INT WINAPI WinMain( HINSTANCE, HINSTANCE, LPSTR, INT )
 {
-	atexit( depgraphV::_atExitFnc );
+	atexit( depgraphV::_atExitFunc );
 	QApplication app( __argc, __argv );
 #else
 } //end of depgraphV namespace
@@ -132,45 +133,52 @@ int main( int argc, char* argv[] )
 
 #endif
 
+	bool logEnabled = false;
 	if( app.arguments().count() > 1 )
 	{
 		//First of all, check for valid option..
 		QStringList validOptions;
-		validOptions << "-h" << "--help" << "-v" << "--version" << "-l" << "--with-log";
-		for( unsigned short i = 1; i < app.arguments().count(); ++i )
+		validOptions << "-h" << "--help" << "-V" << "--version" << "-l" << "--with-log";
+		for( unsigned short i = 1; i < app.arguments().count(); i++ )
 		{
-			if( !validOptions.contains( app.arguments()[ i ] ) )
+			QString current = app.arguments()[ i ];
+			if( !validOptions.contains( current ) )
 			{
 				//Wrong option. Print help and quit
-				printf( "WRONG USAGE: Unknown option \"%s\"\n\n", app.arguments()[ i ].toStdString().c_str() );
+				printf( "WRONG USAGE: Unknown option \"%s\"\n\n", current.toStdString().c_str() );
 				depgraphV::printHelp();
 				return 0;
 			}
-		}
 
-		if( app.arguments().contains( "--version" ) || app.arguments().contains( "-v" ) )
-		{
-			depgraphV::printVersion();
-			return 0;
-		}
+			if( current == "--version" || current == "-V" )
+			{
+				depgraphV::printVersion();
+				return 0;
+			}
 
-		else if( app.arguments().contains( "--help" ) || app.arguments().contains( "-h" ) )
-		{
-			depgraphV::printHelp();
-			return 0;
-		}
+			else if( current == "--help" || current == "-h" )
+			{
+				depgraphV::printHelp();
+				return 0;
+			}
 
-		else if( !app.arguments().contains( "--with-log" ) && !app.arguments().contains( "-l" ) )
-		{
-#ifdef QT_USE_QT5
-			qInstallMessageHandler( &depgraphV::noMessageOutput );
-#else
-			qInstallMsgHandler( &depgraphV::noMessageOutput );
-#endif
+			else if( current == "--with-log" || current == "-l" )
+				logEnabled = true;
 		}
+	}
+
+	if( logEnabled )
+	{
 #ifdef WIN32
-		else
-			depgraphV::toggleConsole( true );
+		depgraphV::toggleConsole( true );
+#endif
+	}
+	else
+	{
+#ifdef QT_USE_QT5
+		qInstallMessageHandler( &depgraphV::noMessageOutput );
+#else
+		qInstallMsgHandler( &depgraphV::noMessageOutput );
 #endif
 	}
 
