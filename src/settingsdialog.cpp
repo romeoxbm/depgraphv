894e56a64283eebbf1a3936c1861935d4ed8f1f5
Francesco Guastella <guastella.francesco@gmail.com>
2014-07-28 17:01:01 +0200

    - New dialog "SettingsDialog" has been added;

---
+++ src/settingsdialog.cpp
@@ -0,0 +1,75 @@
+/**
+ * settingsdialog.cpp
+ *
+ * This source file is part of dep-graphV - An useful tool to analize header
+ * dependendencies via graphs.
+ *
+ * This software is distributed under the MIT License:
+ *
+ * Copyright (c) 2013 - 2014 Francesco Guastella aka romeoxbm
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+#include "settingsdialog.h"
+#include "ui_settingsdialog.h"
+#include "mainwindow.h"
+
+namespace depgraphV
+{
+	SettingsDialog::SettingsDialog( MainWindow* win )
+		: QDialog( static_cast<QWidget*>( win ) ),
+		_ui( new Ui::SettingsDialog )
+	{
+		_ui->setupUi( this );
+	}
+	//--------------------------------------------------------------------------------------------------------------------------
+	SettingsDialog::~SettingsDialog()
+	{
+		delete _ui;
+	}
+	//--------------------------------------------------------------------------------------------------------------------------
+	void SettingsDialog::addPage( const QString& iconPath, const QString& buttonText, QWidget* page )
+	{
+		Q_ASSERT( !iconPath.isEmpty() && !buttonText.isEmpty() && page );
+		
+		QListWidgetItem* button = new QListWidgetItem( _ui->listWidget );
+		button->setIcon( QIcon( iconPath ) );
+		button->setText( buttonText );
+		button->setTextAlignment( Qt::AlignHCenter );
+		button->setFlags( Qt::ItemIsSelectable | Qt::ItemIsEnabled );
+
+		_ui->stackedWidget->addWidget( page );
+	}
+	//--------------------------------------------------------------------------------------------------------------------------
+	void SettingsDialog::changeEvent( QEvent* event )
+	{
+		if( event && event->type() == QEvent::LanguageChange )
+			_ui->retranslateUi( this );
+
+		QDialog::changeEvent( event );
+	}
+	//--------------------------------------------------------------------------------------------------------------------------
+	void SettingsDialog::changePage( QListWidgetItem* current, QListWidgetItem* previous )
+	{
+		if( !current )
+			current = previous;
+
+		_ui->stackedWidget->setCurrentIndex( _ui->listWidget->row( current ) );
+		emit pageChanged( current );
+	}
+}
