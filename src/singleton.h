e3abc3fae130c860ae188cc715de45509ca57520
Francesco Guastella <guastella.francesco@gmail.com>
2014-08-22 01:34:21 +0200

    - Added singleton.h;

---
+++ src/singleton.h
@@ -0,0 +1,72 @@
+/**
+ * singleton.h
+ *
+ * This source file is part of dep-graphV - An useful tool to analize header
+ * dependendencies via graphs.
+ *
+ * This software is distributed under the MIT License:
+ *
+ * Copyright (c) 2013 - 2014 Francesco Guastella aka romeoxbm
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+#ifndef SINGLETON_H
+#define SINGLETON_H
+
+#include <qglobal.h>
+
+namespace depgraphV
+{
+	template<typename T>
+	class Singleton
+	{
+	public:
+		static T& instance()
+		{
+			return *Singleton<T>::instancePtr();
+		}
+
+		static T* instancePtr()
+		{
+			Q_ASSERT( _instance );
+			return _instance;
+		}
+
+	protected:
+		Singleton()
+		{
+			Q_ASSERT( !_instance );
+			_instance = static_cast<T*>( this );
+		}
+
+		~Singleton()
+		{
+			_instance = 0;
+		}
+
+	private:
+		static T* _instance;
+
+		Q_DISABLE_COPY( Singleton )
+	};
+
+	template<typename T>
+	T* Singleton<T>::_instance = 0;
+}
+
+#endif // SINGLETON_H
