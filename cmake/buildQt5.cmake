3187c6d2bf6a5b66b324fd88b4889ea9b055862e
Francesco Guastella <guastella.francesco@gmail.com>
2014-09-27 14:21:04 +0200

    - Disabled AUTOMOC; - Completed support for PCH in cmake scripts; - Autogenerated files are now stored in <build directory>/src; - Updated version of Qt4 used on Windows (4.8.6);

--- cmake/buildQt5.cmake
+++ cmake/buildQt5.cmake
@@ -30,6 +30,83 @@ if( NOT DEFINED qt5Modules )
 	set( qt5Modules "Widgets" )
 endif()
 
+function( CUSTOM_QT5_ADD_RESOURCES outfiles )
+    set( options )
+    set( oneValueArgs )
+    set( multiValueArgs OPTIONS )
+
+    cmake_parse_arguments( _RCC "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN} )
+
+    set( rcc_files ${_RCC_UNPARSED_ARGUMENTS} )
+    set( rcc_options ${_RCC_OPTIONS} )
+
+    foreach( it ${rcc_files} )
+        get_filename_component( outfilename ${it} NAME_WE )
+        get_filename_component( infile ${it} ABSOLUTE )
+        get_filename_component( rc_path ${infile} PATH )
+        set( outfile ${CMAKE_CURRENT_BINARY_DIR}/src/qrc_${outfilename}.cpp )
+
+        set( _RC_DEPENDS )
+        if( EXISTS "${infile}" )
+            #  parse file for dependencies
+            #  all files are absolute paths or relative to the location of the qrc file
+            file( READ "${infile}" _RC_FILE_CONTENTS )
+            string( REGEX MATCHALL "<file[^<]+" _RC_FILES "${_RC_FILE_CONTENTS}" )
+            foreach( _RC_FILE ${_RC_FILES})
+                string( REGEX REPLACE "^<file[^>]*>" "" _RC_FILE "${_RC_FILE}" )
+                if( NOT IS_ABSOLUTE "${_RC_FILE}" )
+                    set( _RC_FILE "${rc_path}/${_RC_FILE}" )
+                endif()
+                set( _RC_DEPENDS ${_RC_DEPENDS} "${_RC_FILE}" )
+            endforeach()
+            # Since this cmake macro is doing the dependency scanning for these files,
+            # let's make a configured file and add it as a dependency so cmake is run
+            # again when dependencies need to be recomputed.
+            qt5_make_output_file( "${infile}" "" "qrc.depends" out_depends )
+            configure_file( "${infile}" "${out_depends}" COPY_ONLY )
+        else()
+            # The .qrc file does not exist (yet). Let's add a dependency and hope
+            # that it will be generated later
+            set( out_depends )
+        endif()
+
+        add_custom_command( OUTPUT ${outfile}
+							COMMAND ${CMAKE_COMMAND}
+							ARGS -E echo "#include \"depgraphv_pch.h\"" > ${outfile}
+							COMMAND ${Qt5Core_RCC_EXECUTABLE}
+							ARGS ${rcc_options} -name ${outfilename} ${infile} >> ${outfile}
+							MAIN_DEPENDENCY ${infile}
+							DEPENDS ${_RC_DEPENDS} "${out_depends}" VERBATIM 
+		)
+        list( APPEND ${outfiles} ${outfile} )
+    endforeach()
+    set( ${outfiles} ${${outfiles}} PARENT_SCOPE )
+endfunction()
+
+function( CUSTOM_QT5_WRAP_UI outfiles )
+    set( options )
+    set( oneValueArgs )
+    set( multiValueArgs OPTIONS )
+
+    cmake_parse_arguments( _WRAP_UI "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN} )
+
+    set( ui_files ${_WRAP_UI_UNPARSED_ARGUMENTS} )
+    set( ui_options ${_WRAP_UI_OPTIONS} )
+
+    foreach( it ${ui_files} )
+        get_filename_component( outfile ${it} NAME_WE )
+        get_filename_component( infile ${it} ABSOLUTE )
+        set( outfile ${CMAKE_CURRENT_BINARY_DIR}/src/ui_${outfile}.h )
+        add_custom_command( OUTPUT ${outfile}
+			COMMAND ${Qt5Widgets_UIC_EXECUTABLE}
+			ARGS ${ui_options} -o ${outfile} ${infile}
+			MAIN_DEPENDENCY ${infile} VERBATIM
+		)
+        list( APPEND ${outfiles} ${outfile} )
+    endforeach()
+    set( ${outfiles} ${${outfiles}} PARENT_SCOPE )
+endfunction()
+
 if( WIN32 )
 	set( QT_INSTALL_PATH "C:/Qt/Qt5.2.1/5.2.1" CACHE PATH "Installation path of Qt libraries." FORCE )
 	if( MSVC12 )
@@ -55,25 +132,35 @@ foreach( mod ${qt5Modules} )
 		message( SEND_ERROR "* Failed to find Qt5 ${mod}." )
 	else()
 		message( STATUS "* Found" )
+		if( NOT mod STREQUAL "LinguistTools" )
+			list( APPEND includes ${Qt5${mod}_INCLUDE_DIRS} )
+			list( APPEND libraries "Qt5::${mod}" )
+		endif()
 	endif()
 endforeach()
 
+if( DEFINED Sources_Moc )
+	qt5_wrap_cpp( Mocs ${Sources_Moc} OPTIONS -bdepgraphv_pch.h )
+endif()
+
 if( DEFINED Sources_ui )
-    qt5_wrap_ui( UISrcs ${Sources_ui} )
+	custom_qt5_wrap_ui( UISrcs ${Sources_ui} )
 endif()
 
 # generate rules for building source files from the resources
 if( DEFINED Resources )
-    qt5_add_resources( RCCSrcs ${Resources} )
+	# generate rules for building source files from the resources
+	if( ${DEPGRAPHV_ENABLE_PCH_SUPPORT} )
+		custom_qt5_add_resources( RCCSrcs ${Resources} )
+	else()
+		qt5_add_resources( RCCSrcs ${Resources} )
+	endif()
 endif()
 
 foreach( t ${AvailableLanguages} )
-    if( DEPGRAPHV_UPDATE_QT_TRANSLATIONS )
-	    qt5_create_translation( i18nSrcs ${FilesToTranslate} ${i18n_${t}} )
-    else()
-	    qt5_add_translation( i18nSrcs ${i18n_${t}} )
-    endif( DEPGRAPHV_UPDATE_QT_TRANSLATIONS )
+	if( DEPGRAPHV_UPDATE_QT_TRANSLATIONS )
+		qt5_create_translation( i18nSrcs ${FilesToTranslate} ${i18n_${t}} )
+	else()
+		qt5_add_translation( i18nSrcs ${i18n_${t}} )
+	endif( DEPGRAPHV_UPDATE_QT_TRANSLATIONS )
 endforeach()
-
-set( includes "${CMAKE_SOURCE_DIR}/src;${GraphViz_INCLUDE_DIRS}" )
-set( libraries "${GraphViz_gvc_LIBRARY}" )
