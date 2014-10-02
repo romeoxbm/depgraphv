2bdd59a2784f6fff712bf6146b57c9a951aad3bc
Francesco Guastella <guastella.francesco@gmail.com>
2014-10-02 21:29:46 +0200

    - Moved ComboBoxItemDelegate;

---
+++ src/comboboxitemdelegate.h
@@ -0,0 +1,44 @@
+/**
+ * comboboxitemdelegate.h
+ *
+ * This source file is part of dep-graphV - An useful tool to analize header
+ * dependendencies via graphs.
+ *
+ * This software is distributed under the MIT License:
+ *
+ * Copyright (c) 2013 - 2014 Francesco Guastella aka romeoxbm
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+#ifndef COMBOBOXITEMDELEGATE_H
+#define COMBOBOXITEMDELEGATE_H
+
+namespace depgraphV
+{
+	class ComboBoxItemDelegate : public QItemDelegate
+	{
+		Q_OBJECT
+
+	public:
+		explicit ComboBoxItemDelegate( QObject* parent = 0 );
+
+		void setEditorData( QWidget* editor, const QModelIndex& index ) const;
+		void setModelData( QWidget* editor, QAbstractItemModel* model, const QModelIndex& index ) const;
+	};
+}
+#endif // COMBOBOXITEMDELEGATE_H
