bb44dba9b3030db73748bf5329ed4f59d02d644c
Francesco Guastella <guastella.francesco@gmail.com>
2014-10-27 16:46:10 +0100

    - Added GeneralPage settings page; - Removed Sql support from CMakeLists.txt; - Added BinaryRadioWidget, CustomItemDelegate and GeneralPage in CMakeLists.txt;

---
+++ src/generalpage.cpp
@@ -0,0 +1,64 @@
+/**
+ * generalpage.cpp
+ *
+ * This source file is part of dep-graphV - An useful tool to analize header
+ * dependendencies via graphs.
+ *
+ * This software is distributed under the MIT License:
+ *
+ * Copyright (c) 2013 - 2014 Francesco Guastella aka romeoxbm
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+#include "depgraphv_pch.h"
+#include "generalpage.h"
+#include "ui_generalpage.h"
+
+namespace depgraphV
+{
+	GeneralPage::GeneralPage( MainWindow* w, SettingsDialog* parent )
+		: SettingsPage( w, parent ),
+		_ui( new Ui::GeneralPage )
+	{
+		_ui->setupUi( this );
+
+		AppConfig* c = Singleton<AppConfig>::instancePtr();
+		connect( _ui->warnOnGraphRemoval, SIGNAL( toggled( bool ) ),
+				 c, SLOT( setWarnOnGraphRemoval( bool ) )
+		);
+	}
+	//-------------------------------------------------------------------------
+	GeneralPage::~GeneralPage()
+	{
+		delete _ui;
+	}
+	//-------------------------------------------------------------------------
+	QString GeneralPage::iconPath() const
+	{
+		//TODO Icon for General Settings
+		return ":/settingsDlgIcons/folder-scan_96x96.png";
+	}
+	//-------------------------------------------------------------------------
+	bool GeneralPage::event( QEvent* evt )
+	{
+		if( evt && evt->type() == QEvent::LanguageChange )
+			_ui->retranslateUi( this );
+
+		return QWidget::event( evt );
+	}
+} // end of depgraphV namespace
---
+++ src/generalpage.h
@@ -0,0 +1,75 @@
+/**
+ * generalpage.h
+ *
+ * This source file is part of dep-graphV - An useful tool to analize header
+ * dependendencies via graphs.
+ *
+ * This software is distributed under the MIT License:
+ *
+ * Copyright (c) 2013 - 2014 Francesco Guastella aka romeoxbm
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+#ifndef GENERALPAGE_H
+#define GENERALPAGE_H
+
+#ifndef SETTINGSPAGE_H
+#	include "settingspage.h"
+#endif
+
+namespace depgraphV
+{
+	namespace Ui
+	{
+		class GeneralPage;
+	}
+
+	class GeneralPage : public SettingsPage
+	{
+		Q_OBJECT
+
+	public:
+		/**
+		 * @brief GeneralPage constructor.
+		 * @param w MainWindow pointer
+		 * @param parent The parent widget (default NULL)
+		 */
+		explicit GeneralPage( MainWindow* w, SettingsDialog* parent = 0 );
+
+		/**
+		 * @brief GeneralPage destructor.
+		 */
+		~GeneralPage();
+
+		virtual QString iconPath() const;
+
+	protected:
+		virtual bool event( QEvent* evt );
+
+	signals:
+
+	public slots:
+
+	private slots:
+
+	private:
+		Ui::GeneralPage* _ui;
+	};
+}
+
+#endif // SCANMODEPAGE_H
